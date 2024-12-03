10781133385d93b0c7f2c99206c44aa204069276
zszillat <zachary.szillat@outlook.com>
2024-12-03 11:07:29 -0500

    minor updates

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,11 +1,9 @@
 alias up='sudo pacman -Syu'
 alias pac='sudo pacman -S '
-alias cdscripts='cd ~/.local/share/Scripts'
+alias cdscripts='cd ~/.local/share/Linux-Scripts'
 alias script='cdscripts && '
 alias addsearch='script ./dmenu_add_to_search.sh'
 alias server='ssh root@192.168.86.126'
 alias update='sudo pacman -Syu'
 alias fixscreen='script ./screen_config.sh'
-alias hi='echo whatsup?'
 
-alias send='websocat ws://localhost:8081'
